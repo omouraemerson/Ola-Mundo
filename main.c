f44381d8aacf876a020f555581f47b3c6bb19246
omouraemerson <oliveiraemoura@gmail.com>
2026-02-27 14:52:51 -0300

    teste
    teste

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero=0;
+
+    printf("digite um valor:\n");
+    scanf("%d",&numero);
+    printf("digitou %d\n",numero);
+    
+    
+    
+    return 0;
+}
